314b3f52fbc36aeaac9edf93441eee658d182035
Victor Moneratto <victor.moneratto@gmail.com>
2019-08-17 22:59:05 -0300

    Dielectric that always tries to refract

--- src/material.rs
+++ src/material.rs
@@ -7,6 +7,7 @@ use rand::distributions::{UnitSphereSurface, Distribution};
 pub enum Material {
     Lambert { albedo: Vec3 },
     Metal { albedo: Vec3, fuzz: f32 },
+    Dielectric { albedo: Vec3, ref_idx: f32 }
 }
 
 fn random_in_unit_sphere() -> Vec3 {
@@ -17,6 +18,18 @@ fn random_in_unit_sphere() -> Vec3 {
 
 fn reflect(v: Vec3, n: Vec3) -> Vec3 {
     v - 2.0 * Vec3::dot(v, n) * n
+}
+
+fn refract(v: &Vec3, n: &Vec3, ni_over_nt: f32) -> Option<Vec3> {
+    let v_norm = v.normalize();
+    let dot = Vec3::dot(v_norm, *n);
+    let delta = 1.0 - ni_over_nt * ni_over_nt * (1.0 - dot * dot);
+
+    if delta > 0.0 {
+        Some(ni_over_nt * (v_norm - *n * dot) - *n * delta.sqrt())
+    } else {
+        None
+    }
 }
 
 impl Material {
@@ -35,6 +48,23 @@ impl Material {
                     (albedo, None)
                 }
             },
+            Material::Dielectric { albedo, ref_idx } => {
+                let outward_normal;
+                let ni_over_nt;
+                if Vec3::dot(r.dir, hit.normal) > 0.0 {
+                    outward_normal = -hit.normal;
+                    ni_over_nt = ref_idx;
+                } else {
+                    outward_normal = hit.normal;
+                    ni_over_nt = 1.0 / ref_idx;
+                }
+
+                if let Some(refract_dir) = refract(&r.dir, &outward_normal, ni_over_nt) {
+                    return (albedo, Some(Ray::new(hit.pos, refract_dir)))
+                }
+
+                (albedo, Some(Ray::new(hit.pos, reflect(r.dir, hit.normal))))
+            }
         }
     }
 }

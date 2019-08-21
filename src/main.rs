7c99130e253c73017cb9904e99afda1ffaee26aa
Victor Moneratto <victor.moneratto@gmail.com>
2019-08-21 01:05:48 -0300

    Output to tga

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,11 @@ use crate::ray::Ray;
 use glam::f32::Vec3;
 use glam::Vec2;
 use std::error::Error;
-use std::fmt::Write as FmtWrite;
 use std::fs::File;
 use std::io::Write as IOWrite;
+use std::fmt::Write;
+use std::time::Instant;
+use std::ops::Div;
 
 mod camera;
 mod hit;
@@ -36,11 +38,6 @@ fn trace(r: &Ray, world: &Vec<Sphere>, depth: i32) -> Vec3 {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut buffer = String::new();
-    writeln!(&mut buffer, "P3").unwrap();
-    writeln!(&mut buffer, "{} {}", VIEW_DIMENSIONS[0], VIEW_DIMENSIONS[1]).unwrap();
-    writeln!(&mut buffer, "255").unwrap();
-
     let lookat = Vec3::new(0.0, 0.0, -1.0);
     let lookfrom = Vec3::new(3.0, 3.0, 2.0);
 
@@ -72,10 +69,14 @@ fn main() -> Result<(), Box<dyn Error>> {
         ),
     ];
 
+    let mut buffer = vec![0u8; (4 * VIEW_DIMENSIONS[0] * VIEW_DIMENSIONS[1]) as usize];
+
+    let instant_before_tracing = Instant::now();
     for y in (0..VIEW_DIMENSIONS[1]).rev() {
         for x in 0..VIEW_DIMENSIONS[0] {
             let mut color = Vec3::zero();
-            for _ in 0..SAMPLES {
+            for _ in 0..SAMPLES
+                {
                 let offset = Vec2::new(rand::random(), rand::random());
                 let uv = Vec2::new(
                     (offset.x() + x as f32) / VIEW_DIMENSIONS[0] as f32,
@@ -95,17 +96,43 @@ fn main() -> Result<(), Box<dyn Error>> {
                 color.z().powf(1.0 / GAMMA),
             );
 
-            writeln!(
-                &mut buffer,
-                "{} {} {}",
+            let base: usize = 4 * (y * VIEW_DIMENSIONS[0] + x) as usize;
+            let (r, g, b, a) = (
                 (color.x() * 255.99) as u8,
                 (color.y() * 255.99) as u8,
-                (color.z() * 255.99) as u8
-            )
-            .unwrap();
+                (color.z() * 255.99) as u8,
+                255 as u8);
+            buffer[base + 0] = b;
+            buffer[base + 1] = g;
+            buffer[base + 2] = r;
+            buffer[base + 3] = a;
         }
     }
 
-    File::create("asd.ppm")?.write((buffer).as_ref())?;
+    let time_elapsed_tracing = instant_before_tracing.elapsed();
+    let time_per_pixel = time_elapsed_tracing.div((VIEW_DIMENSIONS[0] * VIEW_DIMENSIONS[1]) as u32);
+    let time_per_sample = time_per_pixel.div(SAMPLES as u32);
+
+    println!("{:>6.2} seconds", time_elapsed_tracing.as_millis() as f32 * 1e-3);
+    println!("{:>6.2} micros/pixel", time_per_pixel.as_nanos() as f32 * 1e-3);
+    println!("{:>6} nanos/sample", time_per_sample.as_nanos() as f32);
+
+    let tga_header : Vec<u8> = vec![
+            0, // ID length
+            0, // no color map
+            2, // uncompressed, true color
+            0, 0, 0, 0, 0,
+            0, 0, 0, 0, // x and y origin
+            (VIEW_DIMENSIONS[0] & 0x00FF) as u8,
+            ((VIEW_DIMENSIONS[0] & 0xFF00) >> 8) as u8,
+            (VIEW_DIMENSIONS[1] & 0x00FF) as u8,
+            ((VIEW_DIMENSIONS[1] & 0xFF00) >> 8) as u8,
+            32, // 32 bit bitmap
+            0
+    ];
+
+    let mut file = File::create("output.tga")?;
+    file.write(tga_header.as_ref())?;
+    file.write(buffer.as_ref())?;
     Ok(())
 }
